fef6d370f4318dbdd15863dcaf8f0f9b69dbd8d6
arthurxavier <arthurxaviergiffoni@gmail.com>
2017-06-08 19:58:42 +0000

    Adding api version contraint to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
-# require 'api_version_constraint'
+require 'api_version_constraint'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions' }
+# devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
-    # namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
     #   resources :users, only: [:show, :create, :update, :destroy]
     #   resources :sessions, only: [:create, :destroy]
     #   resources :tasks, only: [:index, :show, :create, :update, :destroy]
-    # end
+    end
   end
 
 end
