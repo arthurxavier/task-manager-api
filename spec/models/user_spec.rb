9ebc3bc739a8fa1f67a64ce3b3486abddc495c09
arthurxavier <arthurxaviergiffoni@gmail.com>
2017-06-08 21:09:25 +0000

    Adding shoulda matcher for user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,45 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build(:user)}
+  let(:user) { build(:user) }
 
-  it{ expect(@user).to respond_to(:email)}
-  it{ expect(@user).to respond_to(:password)}
-  it{ expect(@user).to respond_to(:password_confirmation)}
-  it{ expect(@user).to be_valid }
-  # let(:user) { build(:user) }
+  it { is_expected.to validate_presence_of(:email) }                      #Nao pode ter usuario com email nill
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }   #Verifica se o email é unico. Nao considera case_insensitive
+  it { is_expected.to validate_confirmation_of(:password) }               #verifica se esta validando o password com o password_confirmation
+  it { is_expected.to allow_value('costa@nonato.com').for(:email) }       #valida esse formato de email
 
-  # it { is_expected.to have_many(:tasks).dependent(:destroy) }
-
-  # it { is_expected.to validate_presence_of(:email) }
-  # it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
-  # it { is_expected.to validate_confirmation_of(:password) }
-  # it { is_expected.to allow_value('costa@nonato.com').for(:email) }
-  # it { is_expected.to validate_uniqueness_of(:auth_token) }
-
-  # describe '#info' do
-  #   it 'returns email, created_at and a Token' do
-  #     user.save!
-  #     allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
-
-  #     expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: abc123xyzTOKEN")
-  #   end
-  # end
-
-  # describe '#generate_authentication_token!' do
-  #   it 'generates a unique auth token' do
-  #     allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
-  #     user.generate_authentication_token!
-
-  #     expect(user.auth_token).to eq('abc123xyzTOKEN')
-  #   end
-
-  #   it 'generates another auth token when the current auth token already has been taken' do
-  #     allow(Devise).to receive(:friendly_token).and_return('abc123tokenxyz', 'abc123tokenxyz', 'abcXYZ123456789')
-  #     existing_user = create(:user)
-  #     user.generate_authentication_token!
-
-  #     expect(user.auth_token).not_to eq(existing_user.auth_token)
-  #   end
-  # end
 end
